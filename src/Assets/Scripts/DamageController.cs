48c93f8152597d7e345d99e77ede6778cd00bae5
Lee Walker <leewalkergm@gmail.com>
2014-12-06 19:49:09 +0000

    Added sounds, lasers and more!

---
+++ src/Assets/Scripts/DamageController.cs
@@ -0,0 +1,106 @@
+﻿using UnityEngine;
+
+public class DamageController : MonoBehaviour
+{
+    /// <summary>
+    /// Health points of the hull
+    /// </summary>
+    public int HullStrength = 1000;
+
+    /// <summary>
+    /// Actual health points of the shield
+    /// </summary>
+    public int ShieldStrength = 1000;
+
+    private int _shieldStrength = 0;
+
+    /// <summary>
+    /// Measured in milliseconds?
+    /// </summary>
+    public int ShieldRegenTime = 1000;
+
+    private float shieldTimer;
+    private bool shieldTimerActive;
+
+    void Start()
+    {
+        shieldTimer = 0;
+        shieldTimerActive = false;
+
+        _shieldStrength = ShieldStrength;
+    }
+
+    void Update()
+    {
+        if(shieldTimerActive && shieldTimer >= 0)
+        {
+            shieldTimer -= Time.deltaTime;
+        }
+
+        if(shieldTimer <= 0)
+        {
+            shieldTimerActive = false;
+            _shieldStrength = ShieldStrength;
+        }
+    }
+
+    internal void Hit(GunController controller)
+    {
+        int laserStrength = controller.Strength;
+
+        if (_shieldStrength > 0)
+        {
+            //Shield zap noise
+            //If this is the player, play sound "taking-damage.wav"
+
+            if (_shieldStrength <= laserStrength)
+            {
+                //If this is the player, play sound "shields-down.wav"
+                _shieldStrength = 0;
+            }
+            else
+            {
+                _shieldStrength -= laserStrength;
+            }
+
+            if(!shieldTimerActive)
+            {
+                shieldTimer = ShieldRegenTime;
+            }
+
+            laserStrength -= _shieldStrength;
+        }
+
+        if (laserStrength > 0 && HullStrength > 0)
+        {
+            //Hull hit noise
+            laserStrength -= HullStrength;
+
+            if (HullStrength <= laserStrength)
+            {
+                //DEAD MAYN
+                HullStrength = 0;
+            }
+            else if (HullStrength <= 100)
+            {
+                //If this is the player, play sound "hull-breached.wav"
+            }
+        }
+
+        DeathCheck();
+
+    }
+
+    private void DeathCheck()
+    {
+        if (HullStrength <= 0)
+        {
+            Destruct();
+        }
+    }
+
+    private void Destruct()
+    {
+        Destroy(transform.root.gameObject);
+    }
+}
---
+++ src/Assets/Scripts/GunController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunController : MonoBehaviour
+{
+    LineRenderer lineRenderer;
+    
+    public float LaserLength = 100;
+
+    public int Strength = 50;
+
+    // Use this for initialization
+    void Start()
+    {
+        lineRenderer = gameObject.GetComponent<LineRenderer>();
+
+        lineRenderer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            StopCoroutine("PlayerFireLaser");
+            StartCoroutine("PlayerFireLaser");
+        }
+    }
+
+    IEnumerator PlayerFireLaser()
+    {
+        lineRenderer.enabled = true;
+
+        while(Input.GetButton("Fire1"))
+        {
+            FireLaser();
+
+            yield return null;
+        }
+
+        lineRenderer.enabled = false;
+    }
+
+    void FireLaser()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+
+        RaycastHit hit;
+
+        Vector3 endPoint = ray.GetPoint(LaserLength);
+
+        if (Physics.Raycast(ray, out hit, 100) && hit.collider.collider.isTrigger)
+        {
+            endPoint = hit.point;
+        }
+
+        if(hit.collider != null)
+        {
+            DamageController controller = hit.collider.GetComponentInParent<DamageController>();
+
+            if (controller != null)
+            {
+                controller.Hit(this);
+            }
+        }
+
+        lineRenderer.SetPosition(0, ray.origin);
+        lineRenderer.SetPosition(1, endPoint);
+    }
+}
+
